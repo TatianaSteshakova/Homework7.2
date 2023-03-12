ca54b798ce9ec4f4086425b33704462f8ee1e0af
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:21:49 +0300

    Add files via upload

---
+++ Homework7.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void fillArray(int* arr, int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand();
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(0, "Rus");
+    
+    int n, m;
+    cout << "Введите длину двух массивов (через пробел): ";
+    cin >> n >> m;
+
+    int* mas = new int[n];
+    int* mas2 = new int[m];
+
+    fillArray(mas, n);
+    fillArray(mas2, m);
+    
+}
